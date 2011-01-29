f95d568fc2901d48dcb04708919ca347ec27d64f
Spencer Dillard <spencer.dillard@gmail.com>
2011-01-29 17:38:56 -0600

    kinda working client (sometimes)

--- server/lib/defs.js
+++ server/lib/defs.js
@@ -1,7 +1,7 @@
 var Defs = module.exports = {
     Images: {
         "actorTile":    "test_sprites.png:6,4",
-        "baseTile":     "test_sprites.png:7,2",
+        "baseTile":     "test_sprites.png:0,0",
         "spawnTile":    "test_sprites.png:7,2"
     }
 };
--- server/lib/tile.js
+++ server/lib/tile.js
@@ -3,7 +3,7 @@ var TILE_SIZE = [64, 64];
 var Tile = module.exports = function(options) {
     options             = options || {};
     
-    this._image         = options.image         || "empty.png";
+    this._image         = options.image         || "empty.png:0,0";
     this._label         = options.label         || null;
     this._title         = options.title         || "Tile";
     this._description   = options.description   || "Tile";
