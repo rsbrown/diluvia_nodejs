7b3d64baf4a5d064cae93f547dd88dbef7625927
Spencer Dillard <spencer.dillard@gmail.com>
2011-01-30 00:46:14 -0600

    added redirection handling and view update if user is logged in

--- server/app.js
+++ server/app.js
@@ -46,18 +46,28 @@ app.listen(3000);
 
 var socket  = io.listen(app),
     world   = new World();
+    
+var redirectBackOrRoot = function(res) {
+    if (global["store_location"]) {
+        res.redirect(global["store_location"]);
+    } else {
+        res.redirect("/");
+    }
+};
 
 app.get('/', function(req, res){
     global["isAuthenticated"] = false; 
+    global["store_location"] = '/';
     res.render('index');
 });
 
 app.get('/play', function(req, res){
+  global["store_location"] = '/play';
   res.render('world');
 });
 
 app.get ('/auth/twitter', function(req, res, params) {
-    //console.log(req);
+    console.log(req);
     if (!req.session.auth) { req.session.auth = {}; }
     req.authenticate(['twitter'], function(error, authenticated) { 
         //console.log("authenticated: " + authenticated);
@@ -71,8 +81,11 @@ app.get ('/auth/twitter', function(req, res, params) {
                 "HMAC-SHA1");
             oa.getProtectedResource("http://twitter.com/statuses/user_timeline.xml", "GET",
                 req.getAuthDetails()["twitter_oauth_token"], req.getAuthDetails()["twitter_oauth_token_secret"],  function (error, data) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end("<html><h1>Hello! Twitter authenticated user ("+req.getAuthDetails().user.username+")</h1>"+data+ "</html>");
+                    global["username"] = req.getAuthDetails().user.username;
+                    
+                    redirectBackOrRoot(res);
+                    //res.writeHead(200, {'Content-Type': 'text/html'});
+                    //res.end("<html><h1>Hello! Twitter authenticated user ("+req.getAuthDetails().user.username+")</h1>"+data+ "</html>");
                 }
             );
         }
@@ -84,6 +97,7 @@ app.get ('/auth/twitter', function(req, res, params) {
 });
 
 app.get('/edit', function(req, res){
+  global["store_location"] = '/edit';
   res.render('editor');
 });
 
