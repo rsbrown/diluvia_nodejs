adbd98049ef51385b6aad0f3e2893b7eebdebd2d
Rick Branson <rick@diodeware.com>
2011-01-29 22:03:10 -0600

    Now using template stuff

--- server/public/js/canvas.js
+++ server/public/js/canvas.js
@@ -1,5 +1,10 @@
 var Canvas = function(controller, element) {
     this._element       = element;
+    this._container     = element.parentNode;
+    
+    this._element.setAttribute("width", $(this._container).innerWidth());
+    this._element.setAttribute("height", $(this._container).innerHeight());
+    
     this._context       = element.getContext("2d");
     this._controller    = controller;
 };
@@ -8,7 +13,7 @@ Canvas.prototype = {
     paint: function(zoneData, zoneState) {        
         var zoneDims    = zoneState.dimensions,
             layerCount  = zoneDims[2];
-        
+            
         for (var li = 0; li < layerCount; li++) {
             var layer = zoneState.layers[li];
             
--- server/public/js/protocol.js
+++ server/public/js/protocol.js
@@ -33,8 +33,6 @@ Protocol.prototype = {
         var self = this;
         
         if (msg) {
-            console.log(msg);
-            
             if (msg.type == "ZoneData") {
                 var zoneData = this._zoneData = msg.attrs;
                 
