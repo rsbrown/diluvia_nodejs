75c643e24859071d966b74f9aa78aae564a1caba
Spencer Dillard <spencer.dillard@gmail.com>
2011-01-30 00:06:33 -0600

    working on auth

--- server/app.js
+++ server/app.js
@@ -5,9 +5,28 @@ var express = require("express"),
     Env     = require("env"),
     Client  = require("client"),
     World   = require("world"),
-    Account = require("account");
+    Account = require("account"),
+    connect = require("connect"),
+    auth    = require("connect-auth");
+    
+try {
+    var keys = require('./auth_keys');
+    for(var key in keys) {
+        global[key] = keys[key];
+    }
+}
+catch(e) {
+    console.log('Unable to locate the auth_keys.js file');
+    return;
+}
 
+var twitterSecrets = {consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret};
+console.log(twitterSecrets);
 var app = express.createServer();
+
+
+
+
 
 app.use(express.staticProvider(__dirname + "/public"));
 app.set('views', __dirname + '/views');
@@ -15,7 +34,11 @@ app.set('view engine', 'ejs');
 app.use(express.bodyDecoder());
 app.use(express.cookieDecoder());
 app.use(express.session({key: '69c39bb0-2c14-11e0-91fa-0800200c9a66',
-                         secret: 'mooninites7b67c080'}));
+                         secret: 'mooninites7b67c080'
+                         }));
+                         
+app.use(connect.cookieDecoder());
+app.use(auth([auth.Twitter(twitterSecrets)]));
 
 app.listen(3000);
 
@@ -23,11 +46,39 @@ var socket  = io.listen(app),
     world   = new World();
 
 app.get('/', function(req, res){
-  res.render('index');
+    global["isAuthenticated"] = false; 
+    res.render('index');
 });
 
 app.get('/play', function(req, res){
   res.render('world');
+});
+
+app.get ('/auth/twitter', function(req, res, params) {
+    console.log(req);
+    if (!req.session.auth) { req.session.auth = {}; }
+    req.authenticate(['twitter'], function(error, authenticated) { 
+        console.log("authenticated: " + authenticated);
+        if( authenticated ) {
+            var oa= new OAuth("http://twitter.com/oauth/request_token",
+                "http://twitter.com/oauth/access_token",
+                twitterConsumerKey,
+                twitterConsumerSecret,
+                "1.0",
+                null,
+                "HMAC-SHA1");
+            oa.getProtectedResource("http://twitter.com/statuses/user_timeline.xml", "GET",
+                req.getAuthDetails()["twitter_oauth_token"], req.getAuthDetails()["twitter_oauth_token_secret"],  function (error, data) {
+                    res.writeHead(200, {'Content-Type': 'text/html'});
+                    res.end("<html><h1>Hello! Twitter authenticated user ("+req.getAuthDetails().user.username+")</h1>"+data+ "</html>");
+                }
+            );
+        }
+        else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end("<html><h1>Twitter authentication failed :( </h1></html>")
+        }
+    });
 });
 
 socket.on("connection", function(conn) {
