030c6a1ca4c414309b5b9cdfa9a0f7908fffc02b
Wayne <wayne.bills@americanroamer.com>
2011-02-01 01:28:26 -0600

    initial cleanup of server.js, moved routes into routes.js

---
+++ server/lib/routes.js
@@ -0,0 +1,74 @@
+var Routes = module.exports = {
+  '/': function(req, res){
+    this._getUsers();
+    this._preParseRequest(req, res);
+    //req.session["isAuthenticated"] = false;
+    req.session["store_location"] = '/';
+    //setZoneState();
+    res.render('index', {locals: {username: username, flash: req.session.flash}});
+  },
+
+  '/ping': function(req, res) {
+    res.send('pong');
+  },
+
+  // Simple code to set the session and avoid twitter auth
+  '/setSession/:user': function(req, res) {
+    //setUser(req, res, req.params.user);
+    this._preParseRequest(req, res, req.params.user);
+    res.render('index', {locals: {username: username, flash: req.session.flash}});
+  },
+
+  '/play': function(req, res){
+    this._preParseRequest(req, res);
+    req.session["store_location"] = '/play';
+    res.render('world', {locals: {flash: req.session.flash}});
+  },
+
+  '/play/:user': function(req, res) {
+    this._preParseRequest(req, res);
+    req.session["store_location"] = '/play';
+    res.render('world', {locals: {flash: req.session.flash}});
+  },
+
+  '/auth/twitter': function(req, res, params) {
+    this._preParseRequest(req, res);
+    if (!req.session.auth) {
+      req.session.auth = {};
+    }
+    req.authenticate(['twitter'], function(error, authenticated) {
+        //console.log("authenticated: " + authenticated);
+        if( authenticated ) {
+        var oa= new OAuth(
+          "http://twitter.com/oauth/request_token",
+          "http://twitter.com/oauth/access_token",
+          twitterConsumerKey,
+          twitterConsumerSecret,
+          "1.0",
+          null,
+          "HMAC-SHA1");
+        oa.getProtectedResource(
+          "http://twitter.com/statuses/user_timeline.xml",
+          "GET",
+          req.getAuthDetails()["twitter_oauth_token"],
+          req.getAuthDetails()["twitter_oauth_token_secret"],
+          function (error, data) {
+          username = req.session["username"] = req.getAuthDetails().user.username;
+          this._setUser(req, res, req.getAuthDetails().user.username);
+          redirectBackOrRoot(req, res);
+          }
+          );
+        }
+        else {
+          req.session.flash.error = "Twitter authentication failed";
+          res.render('index', {locals: {flash: req.session.flash, username: null}});
+        }
+    });
+  },
+
+  '/edit': function(req, res){
+    this._preParseRequest(req, res);
+    req.session["store_location"] = '/edit';
+    res.render('editor', {locals: {flash: req.session.flash}});
+  }
+};
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,37 +1,115 @@
-var io      = require("socket.io"),
-    Client  = require("client"),
-    World   = require("world"),
-    Account = require("account"),
-    PortalTile = require("portal_tile");
-        
-var Server = module.exports = function(app) {
-    var socket      = io.listen(app),
-        world       = new World();
-    
-    // var defZone     = world.generateDefaultZone(),
-    //     secondZone  = world.generateDefaultZone({ width: 32, height: 32, baseTile: "BASE_STONE" }),
-    //     portalTile1 = defZone.addTile(new PortalTile({ "zone": "zones:2" })),
-    //     portalTile2 = secondZone.addTile(new PortalTile({ "zone": World.DEFAULT_ZONE_ID }));
-    // 
-    // defZone.setLayerTile(1, defZone.xyToIndex(2, 2), portalTile1);
-    // secondZone.setLayerTile(1, secondZone.xyToIndex(4, 4), portalTile2);
-    // 
-    // world.setDefaultZone(defZone);    
-    // world.setZone("zones:2", secondZone);
-    
-    socket.on("connection", function(conn) {
+var io          = require("socket.io"),
+    Client      = require("client"),
+    World       = require("world"),
+    Account     = require("account"),
+    PortalTile  = require("portal_tile"),
+    Util        = require("utilities"),
+    Routes      = require("routes");
+
+var Server = module.exports = function(app, redis) {
+    this.socket     = io.listen(app);
+    var world       = new World();
+    this.redis = redis;
+
+    for (var key in Routes) {
+        app.get(key, Util.bind(Routes[key], this));
+    }
+
+    this.socket.on("connection", function(conn) {
         var account = new Account(world),
-            client  = new Client(conn, account);
+        client  = new Client(conn, account);
 
         world.addAccount(account);
 
         conn.on("message", function(msg) {
-            console.log(msg);
-            client.onMessage(msg);
+          console.log(msg);
+          client.onMessage(msg);
         });
 
         conn.on("disconnect", function() {
-            client.onDisconnect();
+          client.onDisconnect();
         });
     });
+};
+
+
+Server.prototype = {
+    _redirectBackOrRoot: function(req, res) {
+        if (global["store_location"]) {
+            res.redirect(global["store_location"]);
+          } else {
+            res.redirect("/");
+          }
+    },
+
+    _preParseRequest: function(req, res, uname) {
+        this._setUser(req, res, uname);
+        if (!req.session.flash) {
+            req.session.flash = {};
+        }
+        if (!req.session.flash.error) {req.session.flash.error = false;}
+        if (!req.session.flash.warning) {req.session.flash.warning = false;}
+        if (!req.session.flash.info) {req.session.flash.info = false;}
+    },
+
+    _setUser: function(req, res, user) {
+        if (user) {
+            username = req.session["username"] = user;
+        }
+        else if (req.query) {
+            username = req.session["username"] = req.query["un"];
+        }
+        else {
+            username = req.session["username"] = null;
+        }
+
+        if (username) {
+            var uKey = "users:" + username;
+            this.redis.get(uKey, function(err, data) {
+                if (!data) {
+                    this.redis.set(uKey, "{}");
+                }
+                else {
+                    userData = data;
+                }
+            });
+
+            this.redis.llen("users", function(err, user_count) {
+                var self        =  this,
+                    userKnown   = false,
+                    q;
+
+                if (user_count && user_count > 0) {
+                    for (i = 0; i < data; i++) {
+                        this.redis.lindex("users", i, function(err, name) {
+                            if (name.toString() == username) {
+                                self.userKnown = true;
+                            }
+                        });
+                        if (self.userKnown) {
+                            break;
+                        }
+                    }
+                    if (!self.userKnown) {
+                        this.redis.rpush("users", username);
+                    }
+                }
+                else {
+                  this.redis.rpush("users", username);
+                }
+            });
+
+        }
+    },
+
+    _getUsers: function() {
+        this.redis.get("users", function(err, data) {
+            if (!data) {
+                return [];
+            }
+            else {
+                return data;
+            }
+        });
+    }
 };
