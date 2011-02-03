83af27b81b1269b9c737995813d9f230c2b10c6b
Rick Branson <rick@diodeware.com>
2011-02-02 22:19:40 -0600

    Fixed some bugs I was encountering; hope it didn't break production

--- server/lib/auth_controller.js
+++ server/lib/auth_controller.js
@@ -1,5 +1,12 @@
 var auth        = require("connect-auth");
 
+// temp code prevents us from crashing --rb
+var FACEBOOK_ID             = FACEBOOK_ID || null;
+var FACEBOOK_SECRET         = FACEBOOK_SECRET || null;
+var FACEBOOK_CALLBACK       = FACEBOOK_CALLBACK || function() { };
+var TWITTER_CONSUMER_KEY    = TWITTER_CONSUMER_KEY || null;
+var TWITTER_CONSUMER_SECRET = TWITTER_CONSUMER_SECRET || null;
+
 var AuthController = module.exports = function(app) {
     app.use(auth( [
       auth.Facebook({appId : FACEBOOK_ID, appSecret: FACEBOOK_SECRET, scope: "email", callback: FACEBOOK_CALLBACK}),
