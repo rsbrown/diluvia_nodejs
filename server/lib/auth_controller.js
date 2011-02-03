bee12c2830169b34f8b717606dbeda47df6b8706
Rick Branson <rick@diodeware.com>
2011-02-03 12:47:58 -0600

    Added error for old env.js; Cleaned up express connect middleware stack; Added custom staticProvider & revalidate middleware to force a must-revalidate Cache-Control header to eliminate caching issues; static JS/CSS will now be GZIP'd and sent to the client

--- server/lib/auth_controller.js
+++ server/lib/auth_controller.js
@@ -1,13 +1,17 @@
 var auth        = require("connect-auth");
 
-// *************************
-// SORRY, SHOULD HAVE MENTIONED THIS: COPY THESE FROM env.js.example TO env.js -- SB
-// *************************
-// var FACEBOOK_ID             = FACEBOOK_ID || null;
-// var FACEBOOK_SECRET         = FACEBOOK_SECRET || null;
-// var FACEBOOK_CALLBACK       = FACEBOOK_CALLBACK || function() { };
-// var TWITTER_CONSUMER_KEY    = TWITTER_CONSUMER_KEY || null;
-// var TWITTER_CONSUMER_SECRET = TWITTER_CONSUMER_SECRET || null;
+// This is a temporary stopgap -- remove when everyone's development env.js is caught up -- RB
+try { 
+    FACEBOOK_ID;
+    FACEBOOK_SECRET;
+    FACEBOOK_CALLBACK;
+    TWITTER_CONSUMER_KEY;
+    TWITTER_CONSUMER_SECRET;
+} 
+catch (e) {
+    console.log("Your env.js is out of date, go check env.js.example!");
+    process.exit();
+}
 
 var AuthController = module.exports = function(app) {
     app.use(auth( [
